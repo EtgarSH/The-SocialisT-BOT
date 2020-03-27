5120d8a9364021e19829a26287df5d571cb5329f
Etgar <etgar90@gmail.com>
2020-03-27 16:19:37 +0300

    Initialize Anti Groovy Spam Bot

---
+++ anti_groovy_spam_bot.py
@@ -0,0 +1,31 @@
+import discord
+
+from config import DISCORD_BOT_TOKEN
+
+
+class AntiGroovySpamBot(discord.Client):
+    LOGIN_MESSAGE = "Logged in as {}"
+    DIRECT_SPAM_ALERT_MESSAGE = "Please write commands in the Groovy Commands channel!"
+
+    async def on_ready(self):
+        print(self.LOGIN_MESSAGE.format(self.user))
+
+    async def on_message(self, message: discord.Message):
+        if message.content:
+            if self.__is_command(message) and not self.__is_in_groovy_commands_channel(message):
+                private_channel = await self.start_private_message(message.author)
+                await self.send_message(private_channel, self.DIRECT_SPAM_ALERT_MESSAGE)
+                await self.delete_message(message)
+
+    @staticmethod
+    def __is_command(message: discord.Message):
+        return message.content and message.content[0] == '-'
+
+    @staticmethod
+    def __is_in_groovy_commands_channel(message: discord.Message):
+        return message.channel.name == 'groovy-commands'
+
+
+if __name__ == "__main__":
+    bot = AntiGroovySpamBot()
+    bot.run(DISCORD_BOT_TOKEN)
